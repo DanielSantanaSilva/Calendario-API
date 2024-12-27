2c6f84e0817219fbf23e134097db923f444743f3
Daniel Santana Silva <daniel.santana.silva@uni9.edu.br>
2024-12-27 14:48:32 -0300

    hotfix: fixed-db-name

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI, { dbName: 'CALENDARIO' })
+    mongoose.connect(process.env.MONGO_URI, { dbName: 'Calendario' })
     console.log('Conectado ao MongoDB!')
   } catch (err) {
     console.log(err)
